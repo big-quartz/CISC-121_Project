52ea1ef360f06995fbbfea815da23106c6f2864c
big-quartz <24fx68@queensu.ca>
2025-12-08 22:14:49 -0500

    Implement random list generation and selection sort
    Added functions to generate a random list and sort it using selection sort.

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hellow world")
+import random
+
+def random_list(n):
+  return [random.randint(1 , 100) for i in range(n)]
+
+def selection_sort(unsorted):
+  LENGTH = len(unsorted)
+  i = 0
+  while i < LENGTH-1:
+    min = i
+    j = i + 1
+    
+    while j < LENGTH :
+      if unsorted[j] < unsorted[min]:
+        min = j
+      j += 1
+      
+    if min != i:
+      unsorted[i], unsorted[min] = unsorted[min], unsorted[i]
+      
+    i +=1
+  return unsorted
